e41f24dca6f66b4bb378841817c45038fca318f8
Hunter Austin <hjadar15@protonmail.com>
2024-01-11 01:31:23 -0500

    remove resume

--- src/pages/resume.pdf.js
+++
@@ -1,6 +0,0 @@
-export async function GET({ params, request }) {
-    const file = path.join(process.cwd(), 'public', 'resume.pdf');
-    var b = Buffer.from(readFileSync(file, 'binary'), 'binary');
-    response.headers.set('Content-Type', 'application/pdf');
-    return response.end(b);
-}
